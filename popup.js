58d0e0266dc9db003a462492b8e8c90457a19e67
barryyik <66619438+barryyik@users.noreply.github.com>
2021-02-12 17:57:39 +0800

    version 1.0.1 update

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     initializeTable();
+    document.getElementById('creditDiv').insertAdjacentHTML("beforeend", '<p>(Version: '+chrome.runtime.getManifest().version+')</p>');
 });
 
 function initializeTable() {
     const tcpacDisplayDiv = document.getElementById('tcpacDisplayDiv');
-    tcpacDisplayDiv.insertAdjacentHTML("afterbegin", '<table><tr id="tcpacTableRow0"><th>Channel</th><th>Claimed Count (Points)</th><th>Delete Entry</th></tr></table>');
+    tcpacDisplayDiv.insertAdjacentHTML("afterbegin", '<table><tr id="tcpacTableRow0"><th>'+chrome.i18n.getMessage("channel")+'</th><th>'+chrome.i18n.getMessage("claimedcount")+'</th><th>'+chrome.i18n.getMessage("clearrecord")+'</th></tr></table>');
     chrome.storage.local.get('tcpacObj', function (result) {
         let tcpacObj = (result.tcpacObj == undefined) ? {} : result.tcpacObj;
         let tableRowNum = 0;
         let tableFirstRow = document.getElementById('tcpacTableRow'+tableRowNum);
         if(Object.keys(tcpacObj).length > 0) {
             for(var key in tcpacObj) {
-                tableFirstRow.insertAdjacentHTML("afterend", "<tr id='tcpacTableRow"+(tableRowNum+1)+"'><td><a id='tcpacTableKey"+(tableRowNum+1)+"' href='https://www.twitch.tv/"+key+"' target='_blank'>"+key+"</a></td><td>"+tcpacObj[key]+" ("+(tcpacObj[key]*50)+")</td><td><button class='tcpacDelBtn' id='tcpacDelBtn"+(tableRowNum+1)+"'>Delete</button></td></tr>");
+                tableFirstRow.insertAdjacentHTML("afterend", "<tr id='tcpacTableRow"+(tableRowNum+1)+"'><td><a id='tcpacTableKey"+(tableRowNum+1)+"' href='https://www.twitch.tv/"+key+"' target='_blank'>"+key+"</a></td><td>"+tcpacObj[key]+" ("+(tcpacObj[key]*50)+")</td><td><button class='tcpacDelBtn' id='tcpacDelBtn"+(tableRowNum+1)+"'>"+chrome.i18n.getMessage('clear')+"</button></td></tr>");
                 tableRowNum += 1;
                 tableFirstRow = document.getElementById('tcpacTableRow'+tableRowNum);
             }
@@ -40,7 +41,7 @@ function initializeTable() {
             tableFirstRow = document.getElementById('tcpacTableRow'+tableRowNum);
         }
         // button
-        tableFirstRow.insertAdjacentHTML("afterend", "<tr id='tcpacTableRowLast'><td></td><td></td><td><button id='tcpacRestBtn'>Delete All</button></td></tr>");
+        tableFirstRow.insertAdjacentHTML("afterend", "<tr id='tcpacTableRowLast'><td></td><td></td><td><button id='tcpacRestBtn'>"+chrome.i18n.getMessage('clearall')+"</button></td></tr>");
         const deleteBtn = document.getElementById('tcpacRestBtn');
         deleteBtn.addEventListener('click', function() {
             chrome.storage.local.remove('tcpacObj', function(){
