3733a90fc89b7d56ecb91d7dff6ccf4a99f31159
barryyik <66619438+barryyik@users.noreply.github.com>
2021-02-21 16:43:49 +0800

    minimized files

---
+++ Compressed files/content.js
@@ -0,0 +1,1 @@
+setInterval(()=>{const t=document.querySelector(".tw-button--success");if(t){t.click();let c=window.location.pathname.substring(1),e=c.substring(0,c.indexOf("/")),n=0;chrome.storage.local.get("tcpacObj",function(t){(n=null==t.tcpacObj?{}:t.tcpacObj)[e]=null==n[e]?1:n[e]+1,chrome.storage.local.set({tcpacObj:n},function(){})})}},1e3);
