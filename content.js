9781d67368f597cad79abeda02814a4b59e34e61
barryyik <66619438+barryyik@users.noreply.github.com>
2021-02-24 01:34:02 +0800

    version 1.0.3 update

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ setInterval(() => {
     if (button) {
         button.click();
         let addressTemp = window.location.pathname.substring(1);
-        let address = addressTemp.substring(0, addressTemp.indexOf('/'));
+        let address = (addressTemp.indexOf('/') == -1) ? addressTemp : addressTemp.substring(0, addressTemp.indexOf('/'));
         let tcpacObj = 0;
         chrome.storage.local.get('tcpacObj', function (result) {
             tcpacObj = (result.tcpacObj == undefined) ? {} : result.tcpacObj;
