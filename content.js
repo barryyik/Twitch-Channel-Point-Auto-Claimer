2c0c9423ef82a37a4dfba3d5f2cca38b80d2469d
barryyik <66619438+barryyik@users.noreply.github.com>
2021-05-25 13:45:14 +0800

    version 1.0.7 update

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 setInterval(() => {
-    const button = document.querySelector('.ScCoreButtonSuccess-sc-1qn4ixc-5')
+    let button = document.querySelector('.ScCoreButtonSuccess-sc-1qn4ixc-5') || document.querySelector('.VGQNd') || document.querySelector('.claimable-bonus__icon')?.parentElement?.parentElement?.parentElement || getButtonByArielLabel()
     if (!button)
         return
+    if (button.nodeName != 'BUTTON')
+        button = getButtonByArielLabel()
     button.click()
     let address = window.location.pathname.match(/(?<=\/).+?(?=\/|$)/g)
     chrome.storage.local.get('tcpacObj', result => {
@@ -10,3 +12,44 @@ setInterval(() => {
         chrome.storage.local.set({tcpacObj: tcpacObj}, () => {})
     })
 }, 2000)
+
+function getButtonByArielLabel() {
+    const arr = [
+        "Claim Bonus",
+        "Få bonus",
+        "Bonus einfordern",
+        "Reclamar bonificación",
+        "Reclamar bono",
+        "Récupérer un bonus",
+        "Riscatta bonus",
+        "Bónusz igénylése",
+        "Bonus claimen",
+        "Motta bonus",
+        "Odbierz bonus",
+        "Receber bónus",
+        "Resgatar Bônus",
+        "Obține bonusul",
+        "Vyzdvihnúť bonus",
+        "Lunasta bonus",
+        "Hämta bonus",
+        "Nhận thưởng",
+        "Bonusu al",
+        "Vyzvednout bonus",
+        "Διεκδίκηση μπόνους",
+        "Получаване на бонус",
+        "Получить бонус",
+        "เคลมโบนัส",
+        "领取奖励",
+        "領取額外獎勵",
+        "ボーナスを受け取る",
+        "보너스 받기"
+    ]
+    let buttonElement = null
+    arr.some(labelName => {
+        if (!document.querySelector(`[aria-label="${labelName}"]`))
+            return false
+        buttonElement = document.querySelector(`[aria-label="${labelName}"]`)
+        return true
+    })
+    return buttonElement
+}
